0cacefa1aa7a35ade94864ad09b6fefc39dfb7da
Raunak <kumarraunak16723@gmail.com>
2025-01-27 09:29:11 +0530

    fix(single-threaded): align client port with server (8010)

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -10,7 +10,7 @@ import java.net.UnknownHostException;
 public class Client {
     
     public void run() throws UnknownHostException, IOException{
-        int port = 8090;
+        int port = 8010;
         InetAddress address = InetAddress.getByName("localhost");
         Socket socket = new Socket(address, port);
         PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
